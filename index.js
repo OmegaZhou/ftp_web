80a84314d751e65e21d6daf4b053ab23aa20e279
Zhou <playerzhou@icloud.com>
2019-09-27 18:25:02 +0800

    test

--- index.js
+++ index.js
@@ -1,23 +1,114 @@
+const express = require('express')
+const session = require('express-session')
+const fs = require('fs');
+const error = require('./lib/error')
+const Client = require('ftp');
+const API_PATH='/ftp/api/'
+var app = express();
 
-const express=require('express')
-const fs=require('fs');
-const error=require('./lib/error')
-const ftp=require('./lib/ftp');
+app.use(session({
+    secret: 'ftp_web',
+    cookie: {
+        maxAge: 60 * 1000 * 30
+    },
+    saveUninitialized: false,
+    resave: false
+}))
 
-var app=express();
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({extended:false}));
 
+app.get('/ftp',function(req,res){
+    res.redirect('/ftp/login.html');
+})
 
-app.get('/ftp/api/get_dir',function(req,res){
-    ftp.getDir({user:"zhou",password:"zhou1998"},function(err,list){
-        if(err){
-            error.error(err);
-            res.status(500);
-        }else{
-            res.json(list);
+app.get('/ftp/login.html',function(req,res){
+    if(req.session.ftp_connection){
+        if(req.session.ftp_connection.flag){
+            res.redirect('/ftp/index.html')
         }
+    }
+    res.send('login.html');
+})
+
+app.get('/ftp/index.html',function(req,res){
+    if(!req.session.ftp_connection){
+        res.redirect('/ftp/login.html');
+    }
+    if(!req.session.ftp_connection.flag){
+        res.redirect('/ftp/login.html');
+    }
+    res.send('index.html');
+})
+
+app.post(API_PATH+'login',function(req,res){
+    if(req.session.ftp_connection){
+        if(req.session.ftp_connection.flag){
+            req.session.ftp_connection.client.end();
+        }
+    }
+    var user=req.body;
+    var ftp=createFtp();
+    req.session.ftp_connection=ftp;
+    ftp.client.on('ready',function(){
+        ftp.flag=1;
+        res.json(createRes("success"));
+    })
+    ftp.client.on('error',function(err){
+        ftp.flag=0;
+        res.json(err);
+    })
+    ftp.client.on('close',function(err){
+        ftp.flag=0;
+    })
+    ftp.client.on('end',function(err){
+        ftp.flag=0;
+    })
+    ftp.client.connect({user:user.user,password:user.password});
+})
+
+app.use(API_PATH,function(req,res,next){
+    if(req.session.ftp_connection){
+        if(req.session.ftp_connection.flag){
+            next();
+        }
+    }
+    res.json(createRes('Need login'));
+})
+
+
+
+app.get(API_PATH+'get_dir',function(req,res){
+    req.session.ftp_connection.client.list(function(err,list){
+        if(err){
+            res.json(createRes("Error",err));
+        }else{
+            res.json(createRes("success",list));
+        }
+
     })
 })
 
-app.listen(9090,function(){
+app.listen(9090, function () {
     console.log('listen to port 9090');
 })
+
+function createFtp()
+{
+    var ftp=new Object();
+    ftp.client=new Client();
+    ftp.flag=0;
+    return ftp;
+}
+
+function createRes(message,result)
+{
+    var res=new Object();
+    if(message){
+        res.message=message;
+    }
+    if(result){
+        res.result=result;
+    }
+    return res;
+}
