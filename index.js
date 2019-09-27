56bb831440bb21ab9b1501084bc39c7e75311561
Zhou <playerzhou@icloud.com>
2019-09-27 15:46:46 +0800

    test

---
+++ index.js
@@ -0,0 +1,23 @@
+
+const express=require('express')
+const fs=require('fs');
+const error=require('./lib/error')
+const ftp=require('./lib/ftp');
+
+var app=express();
+
+
+app.get('/ftp/api/get_dir',function(req,res){
+    ftp.getDir({user:"zhou",password:"zhou1998"},function(err,list){
+        if(err){
+            error.error(err);
+            res.status(500);
+        }else{
+            res.json(list)
+        }
+    })
+})
+
+app.listen(9090,function(){
+    console.log('listen to port 9090');
+})
