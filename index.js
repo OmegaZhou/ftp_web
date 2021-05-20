36773f861807cada6ffb464cbab876c533ead44d
Zhou <playerzhou@icloud.com>
2021-05-20 21:47:28 +0800

    update rsa

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const session = require('express-session')
 const body_parser = require('body-parser')
-const multer=require('multer')
-const api=require('./lib/api')
-const upload=multer({dest:'./upload_tmp'});
-const createRes=api.createRes;
+const multer = require('multer')
+const api = require('./lib/api')
+const upload = multer({ dest: './upload_tmp' });
+const createRes = api.createRes;
 const API_PATH = '/ftp/api/'
 var app = express();
-var client_map=api.client_map;
+var client_map = api.client_map;
 app.use(session({
     secret: 'ftp_web',
     cookie: {
@@ -19,9 +19,9 @@ app.use(session({
 }))
 
 app.use('/ftp/img', express.static(__dirname + '/img'))
-
-app.use(body_parser.json());
-app.use(body_parser.urlencoded({ extended: false }));
+app.use("/ftp/static_lib", express.static(__dirname + '/static_lib'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/ftp', function (req, res) {
     res.redirect('/ftp/login.html');
@@ -39,7 +39,7 @@ app.get('/ftp/login.html', function (req, res) {
     }
     res.sendFile(__dirname + '/login.html');
 
-} )
+})
 
 app.get('/ftp/index.html', function (req, res) {
     if (!req.session.ftp_connection) {
@@ -54,44 +54,44 @@ app.get('/ftp/index.html', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 })
 
-app.post(API_PATH + 'login',api.login )
-
+app.post(API_PATH + 'login', api.login)
+app.get(API_PATH + "pub_rsa", api.get_pub_rsa)
 app.use(API_PATH, function (req, res, next) {
     if (req.session.ftp_connection) {
         var index = req.session.id;
         if (client_map.has(index)) {
             next();
         }
-    }else{
+    } else {
         res.json(createRes('Need login'));
     }
-    
+
 })
 
 
 
 app.get(API_PATH + 'get_dir', api.getDir)
 
-app.get(API_PATH+'pwd',api.pwd)
+app.get(API_PATH + 'pwd', api.pwd)
 
-app.post(API_PATH+'change_dir',api.cwd);
+app.post(API_PATH + 'change_dir', api.cwd);
 
-app.get(API_PATH+'download/:path',api.download);
+app.get(API_PATH + 'download/:path', api.download);
 
-app.post(API_PATH+'upload',upload.single('file'),api.upload)
-app.post(API_PATH+'mkdir',api.mkdir);
-app.post(API_PATH+'rename',api.rename);
-app.post(API_PATH+'rmdir',api.rmdir);
-app.post(API_PATH+'delete_file',api.delete_file);
-app.get(API_PATH+'logout',function(req,res){
+app.post(API_PATH + 'upload', upload.single('file'), api.upload)
+app.post(API_PATH + 'mkdir', api.mkdir);
+app.post(API_PATH + 'rename', api.rename);
+app.post(API_PATH + 'rmdir', api.rmdir);
+app.post(API_PATH + 'delete_file', api.delete_file);
+app.get(API_PATH + 'logout', function (req, res) {
     var index = req.session.id;
     if (client_map.has(index)) {
         var ftp = client_map.get(index);
         ftp.client.end();
         client_map.delete(index)
     }
-    req.session.destroy(success=>{
-        res.json(createRes("success",success))
+    req.session.destroy(success => {
+        res.json(createRes("success", success))
     });
 })
 app.listen(9090, function () {
